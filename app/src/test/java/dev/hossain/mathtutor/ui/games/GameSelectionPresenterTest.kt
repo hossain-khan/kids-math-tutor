df4c503d7a90dca7c48b1d5c62045f1355f7db82
Hossain Khan <hello@hossain.dev>
2025-12-24 14:39:58 -0500

    [FIXED] Formatting

--- app/src/test/java/dev/hossain/mathtutor/ui/games/GameSelectionPresenterTest.kt
+++ app/src/test/java/dev/hossain/mathtutor/ui/games/GameSelectionPresenterTest.kt
@@ -209,42 +209,54 @@ class GameSelectionPresenterTest {
 
     @Test
     fun `MathRaceScreen defaults to non-trial mode`() {
-        val screen = dev.hossain.mathtutor.ui.mathrace.MathRaceScreen()
+        val screen =
+            dev.hossain.mathtutor.ui.mathrace
+                .MathRaceScreen()
 
         assertThat(screen.isTrialMode).isFalse()
     }
 
     @Test
     fun `MathRaceScreen can be created in trial mode`() {
-        val screen = dev.hossain.mathtutor.ui.mathrace.MathRaceScreen(isTrialMode = true)
+        val screen =
+            dev.hossain.mathtutor.ui.mathrace
+                .MathRaceScreen(isTrialMode = true)
 
         assertThat(screen.isTrialMode).isTrue()
     }
 
     @Test
     fun `MemoryMatchScreen defaults to non-trial mode`() {
-        val screen = dev.hossain.mathtutor.ui.memorymatch.MemoryMatchScreen()
+        val screen =
+            dev.hossain.mathtutor.ui.memorymatch
+                .MemoryMatchScreen()
 
         assertThat(screen.isTrialMode).isFalse()
     }
 
     @Test
     fun `MemoryMatchScreen can be created in trial mode`() {
-        val screen = dev.hossain.mathtutor.ui.memorymatch.MemoryMatchScreen(isTrialMode = true)
+        val screen =
+            dev.hossain.mathtutor.ui.memorymatch
+                .MemoryMatchScreen(isTrialMode = true)
 
         assertThat(screen.isTrialMode).isTrue()
     }
 
     @Test
     fun `NumberSequenceScreen defaults to non-trial mode`() {
-        val screen = dev.hossain.mathtutor.ui.numbersequence.NumberSequenceScreen()
+        val screen =
+            dev.hossain.mathtutor.ui.numbersequence
+                .NumberSequenceScreen()
 
         assertThat(screen.isTrialMode).isFalse()
     }
 
     @Test
     fun `NumberSequenceScreen can be created in trial mode`() {
-        val screen = dev.hossain.mathtutor.ui.numbersequence.NumberSequenceScreen(isTrialMode = true)
+        val screen =
+            dev.hossain.mathtutor.ui.numbersequence
+                .NumberSequenceScreen(isTrialMode = true)
 
         assertThat(screen.isTrialMode).isTrue()
     }
