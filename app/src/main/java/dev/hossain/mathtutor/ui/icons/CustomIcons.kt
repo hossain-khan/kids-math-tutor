21ce1da976d329303334540cbe39e1b2b47d4b0e
Hossain Khan <hello@hossain.dev>
2026-01-01 11:40:03 -0500

    [FIXED] Formatter

--- app/src/main/java/dev/hossain/mathtutor/ui/icons/CustomIcons.kt
+++ app/src/main/java/dev/hossain/mathtutor/ui/icons/CustomIcons.kt
@@ -23,31 +23,33 @@ object CustomIcons {
             if (_Division != null) {
                 return _Division!!
             }
-            _Division = ImageVector.Builder(
-                name = "Division",
-                defaultWidth = 800.dp,
-                defaultHeight = 800.dp,
-                viewportWidth = 19f,
-                viewportHeight = 19f
-            ).apply {
-                path(fill = SolidColor(Color.Black)) {
-                    moveTo(15.711f, 9.182f)
-                    arcToRelative(1.03f, 1.03f, 0f, isMoreThanHalf = false, isPositiveArc = true, -1.03f, 1.03f)
-                    lineTo(4.319f, 10.212f)
-                    arcToRelative(1.03f, 1.03f, 0f, isMoreThanHalf = true, isPositiveArc = true, 0f, -2.059f)
-                    horizontalLineToRelative(10.364f)
-                    arcToRelative(1.03f, 1.03f, 0f, isMoreThanHalf = false, isPositiveArc = true, 1.029f, 1.03f)
-                    close()
-                    moveTo(8.03f, 4.586f)
-                    arcToRelative(1.47f, 1.47f, 0f, isMoreThanHalf = true, isPositiveArc = true, 1.47f, 1.47f)
-                    arcToRelative(1.47f, 1.47f, 0f, isMoreThanHalf = false, isPositiveArc = true, -1.47f, -1.47f)
-                    close()
-                    moveTo(10.97f, 13.779f)
-                    arcToRelative(1.47f, 1.47f, 0f, isMoreThanHalf = true, isPositiveArc = true, -1.47f, -1.47f)
-                    arcToRelative(1.47f, 1.47f, 0f, isMoreThanHalf = false, isPositiveArc = true, 1.47f, 1.47f)
-                    close()
-                }
-            }.build()
+            _Division =
+                ImageVector
+                    .Builder(
+                        name = "Division",
+                        defaultWidth = 800.dp,
+                        defaultHeight = 800.dp,
+                        viewportWidth = 19f,
+                        viewportHeight = 19f,
+                    ).apply {
+                        path(fill = SolidColor(Color.Black)) {
+                            moveTo(15.711f, 9.182f)
+                            arcToRelative(1.03f, 1.03f, 0f, isMoreThanHalf = false, isPositiveArc = true, -1.03f, 1.03f)
+                            lineTo(4.319f, 10.212f)
+                            arcToRelative(1.03f, 1.03f, 0f, isMoreThanHalf = true, isPositiveArc = true, 0f, -2.059f)
+                            horizontalLineToRelative(10.364f)
+                            arcToRelative(1.03f, 1.03f, 0f, isMoreThanHalf = false, isPositiveArc = true, 1.029f, 1.03f)
+                            close()
+                            moveTo(8.03f, 4.586f)
+                            arcToRelative(1.47f, 1.47f, 0f, isMoreThanHalf = true, isPositiveArc = true, 1.47f, 1.47f)
+                            arcToRelative(1.47f, 1.47f, 0f, isMoreThanHalf = false, isPositiveArc = true, -1.47f, -1.47f)
+                            close()
+                            moveTo(10.97f, 13.779f)
+                            arcToRelative(1.47f, 1.47f, 0f, isMoreThanHalf = true, isPositiveArc = true, -1.47f, -1.47f)
+                            arcToRelative(1.47f, 1.47f, 0f, isMoreThanHalf = false, isPositiveArc = true, 1.47f, 1.47f)
+                            close()
+                        }
+                    }.build()
 
             return _Division!!
         }
@@ -55,6 +57,3 @@ object CustomIcons {
     @Suppress("ObjectPropertyName")
     private var _Division: ImageVector? = null
 }
-
-
-
